2eca15b4b7868e722ec3f91f3e4540ab2cf87704
isanchezdy <75845965+isanchezdy@users.noreply.github.com>
2022-11-21 12:11:15 +0100

    PGD05-151 IT07 - IMP - CU09 - Implement components - DS #done #time 1d 14m

---
+++ app/src/main/java/es/unex/dcadmin/commandRecord/CommandRecordList.java
@@ -0,0 +1,121 @@
+package es.unex.dcadmin.commandRecord;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import es.unex.dcadmin.AppExecutors;
+import es.unex.dcadmin.R;
+import es.unex.dcadmin.roomdb.AppDatabase;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CommandRecordList#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CommandRecordList extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private CommandRecordAdapter mAdapter;
+
+    public CommandRecordList() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CommandRecordList.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CommandRecordList newInstance(String param1, String param2) {
+        CommandRecordList fragment = new CommandRecordList();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        loadItems(); //Para cargar los datos
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.command_record, container, false);
+
+        mRecyclerView = v.findViewById(R.id.commandRecyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new CommandRecordAdapter(new CommandRecordAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(CommandRecord item) {
+
+            }
+        }, new CommandRecordAdapter.OnDeleteClickListener() {
+            @Override
+            public void onDeleteClick(CommandRecord item) {
+
+            }
+        });
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                CommandRecord cr = new CommandRecord("Nombre",3,"UsuarioTest");
+                AppDatabase.getInstance(getActivity()).getCommandRecordDao().insert(cr);
+                getActivity().runOnUiThread(() -> mAdapter.add(cr));
+            }
+        });
+
+        return v;
+    }
+
+    private void loadItems() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                List<CommandRecord> items = AppDatabase.getInstance(getActivity()).getCommandRecordDao().getAll();
+                getActivity().runOnUiThread(() ->mAdapter.load(items));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/es/unex/dcadmin/roomdb/CommandRecordDao.java
@@ -0,0 +1,32 @@
+package es.unex.dcadmin.roomdb;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import es.unex.dcadmin.commandRecord.CommandRecord;
+
+@Dao
+public interface CommandRecordDao {
+    @Query("SELECT * FROM commandRecord")
+    public List<CommandRecord> getAll();
+
+    @Query("SELECT * FROM commandRecord WHERE name = :commandName and userId = :commandUserId")
+    public CommandRecord get(String commandName, String commandUserId);
+
+    @Insert
+    public long insert(CommandRecord item);
+
+    @Query("DELETE FROM commandRecord")
+    public void deleteAll();
+
+    @Update
+    public int update(CommandRecord item);
+
+    @Delete
+    public int delete(CommandRecord item);
+}
