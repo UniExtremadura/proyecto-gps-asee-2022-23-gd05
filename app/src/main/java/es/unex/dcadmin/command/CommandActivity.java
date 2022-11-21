d0237178f3a8cd4265a7e8c2210754710b3a4bc3
ralvaradx <ralvaradx@alumnos.unex.es>
2022-11-21 11:29:50 +0100

    PGD05-165 IT07 - IMP - CU10 - Implement components - DJ #done #time 1d 14m

---
+++ app/src/main/java/es/unex/dcadmin/command/CommandActivity.java
@@ -0,0 +1,122 @@
+package es.unex.dcadmin.command;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+import es.unex.dcadmin.R;
+import es.unex.dcadmin.commandRecord.CommandRecordList;
+
+public class CommandActivity extends AppCompatActivity {
+    //La activity implementa el callback
+
+    private static final int ADD_TODO_ITEM_REQUEST = 0;
+
+    private static final String FILE_NAME = "TodoManagerActivityData.txt";
+    private static final String TAG = "Lab-UserInterface";
+
+    // IDs for menu items
+    private static final int MENU_DELETE = Menu.FIRST;
+    private static final int MENU_DUMP = Menu.FIRST + 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command);
+
+        ImageView command_list = findViewById(R.id.command_record_list); //Lista de comandos
+        command_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CommandRecordList fragment = new CommandRecordList();
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_to_do_manager, fragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Save Commands
+
+        //saveItems();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*super.onCreateOptionsMenu(menu);
+
+        menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all");
+        menu.add(Menu.NONE, MENU_DUMP, Menu.NONE, "Dump to log");*/
+        return true;
+    }
+
+    private void dump() {
+    }
+
+    // Load stored ToDoItems
+    private void loadItems() {
+    }
+
+    // Save ToDoItems to file
+    /*private void saveItems() {
+        PrintWriter writer = null;
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    fos)));
+
+            for (int idx = 0; idx < mAdapter.getItemCount(); idx++) {
+
+                writer.println(mAdapter.getItem(idx));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != writer) {
+                writer.close();
+            }
+        }
+    }*/
+
+    private void log(String msg) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i(TAG, msg);
+    }
+
+    /*@Override
+    public void onBackPressed(){ //Para no acceder a la pantalla de añadir token si ya lo hemos añadido
+        moveTaskToBack(true);
+    }*/
+
+}
+
---
+++ app/src/main/java/es/unex/dcadmin/commandRecord/CommandRecordAdapter.java
@@ -0,0 +1,148 @@
+package es.unex.dcadmin.commandRecord;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.unex.dcadmin.R;
+
+public class CommandRecordAdapter extends RecyclerView.Adapter<CommandRecordAdapter.ViewHolder> {
+    private List<CommandRecord> mItems = new ArrayList<CommandRecord>();
+
+    public interface OnItemClickListener {
+        void onItemClick(CommandRecord item);     //Type of the element to be returned
+    }
+
+    public interface OnDeleteClickListener{ //Interfaz del listener de borrar
+        void onDeleteClick(CommandRecord item);
+    }
+
+    private final OnItemClickListener listener;
+    private final OnDeleteClickListener deleteListener; //Listener para borrar
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CommandRecordAdapter(OnItemClickListener listener, OnDeleteClickListener deleteListener) {//El segundo parametro es para el listener de borrar
+
+        this.listener = listener;
+        this.deleteListener = deleteListener; //Listener de borrar
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,//Parent es la vista padre de este elemento, en este caso la recyclerview, es el que contiene este elemento
+                                                                              int viewType) {
+        //TODO - Inflate the View for every element
+        //A partir de un layout, le metemos los datos a la vista(lo mismo que hacíamos con los fragments, crear una vista)
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.command_record_item,parent,false);//LayoutInflater solo se puede crear con from. Esto mete los datos en el layout todo_item
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(mItems.get(position),listener, deleteListener); //Este último parametro es para el listener de borrar
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void add(CommandRecord item) {
+
+        mItems.add(item);
+        notifyDataSetChanged();
+
+    }
+
+    public void clear(){
+
+        mItems.clear();
+        notifyDataSetChanged();
+
+    }
+
+    public Object getItem(int pos) {
+
+        return mItems.get(pos);
+
+    }
+
+    public void load(List<CommandRecord> items){
+
+        mItems.clear();
+        mItems = items;
+        notifyDataSetChanged();
+
+    }
+
+    public void update(CommandRecord item){
+        int pos = -1;
+        boolean found = false;
+        for(int i = 0;i<mItems.size() && !found; i++){
+            if(mItems.get(i).getId() == item.getId()){
+                pos = i;
+                found = true;
+            }
+        }
+
+        if(pos != -1)
+            mItems.set(pos,item);
+
+        notifyDataSetChanged();
+
+    }
+
+    public void delete(CommandRecord item){
+        int pos = mItems.indexOf(item);
+        mItems.remove(item);
+
+        notifyItemRemoved(pos);
+        notifyItemRangeChanged(pos,mItems.size());
+
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private TextView user;
+        private TextView numExecutions;
+
+        public ViewHolder(View itemView) {//itemVIew es la vista que contiene a todos los elementos
+            super(itemView);
+
+            //TODO - Get the references to every widget of the Item View
+            name = itemView.findViewById(R.id.commandName);
+            user = itemView.findViewById(R.id.command_user);
+            numExecutions = itemView.findViewById(R.id.command_num_executions);
+        }
+
+        public void bind(final CommandRecord toDoItem, final OnItemClickListener listener, final OnDeleteClickListener deleteListener) { //Este ultimo parametro es para el listener de borrar
+            //Vamos a vincular las vistas con los datos de toDoItem
+            //TODO - Display Title in TextView
+            name.setText(toDoItem.getName());
+
+            user.setText(toDoItem.getUserId());
+
+            numExecutions.setText(String.valueOf(toDoItem.getNumExecutions()));
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(toDoItem);
+                }
+            });
+        }
+    }
+
+}
+
