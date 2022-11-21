0f60a6e39329efb218fed205933c988f7118b6b3
ralvaradx <ralvaradx@alumnos.unex.es>
2022-11-21 11:40:55 +0100

    PGD05-186 IT08 - IMP - CU11 - Implement components - DS #done #time 1d 14m

--- app/src/main/java/es/unex/dcadmin/MainActivity.java
+++ app/src/main/java/es/unex/dcadmin/MainActivity.java
@@ -1,23 +1,64 @@
 package es.unex.dcadmin;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.w3c.dom.Text;
+
+import es.unex.dcadmin.discord.discordApiManager;
 
 public class MainActivity extends AppCompatActivity {
+    public static TextView mensaje;
+    public static ProgressBar progressBar;
+    public static View.OnClickListener listener;
+    public static View layout;
+
+    public static View command_b;
+    public static TextView addTokenView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bot_token);
+
+        addTokenView = (TextView) findViewById(R.id.addTokenView);
+
+        mensaje = findViewById(R.id.infoMessage);
+        progressBar = findViewById(R.id.progressBar);
+
+        layout = findViewById(R.id.entireScreen);
+        listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mensaje.setText("El bot ha sido encendido.");
+            }
+        };
+
+        command_b = findViewById(R.id.access);
+        command_b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Si ya hay token en preferencias, no guarda, si no, guardar token
+                command_b.setClickable(false);
+                addTokenView.setClickable(false);
+                progressBar.setVisibility(View.VISIBLE);
+
+                discordApiManager.setToken(addTokenView.getText().toString());
+                // cargando... -> pantalla de cargando
+                discordApiManager.getSingleton();
+            }
+        });
 
     }
 
