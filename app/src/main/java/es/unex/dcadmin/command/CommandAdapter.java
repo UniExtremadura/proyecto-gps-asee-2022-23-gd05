441c4b0cb126a1900b5ecd9fc52e64ad5c3de7ed
jorgecs <jcascose@alumnos.unex.es>
2022-11-21 11:23:50 +0100

    PGD05-89 IT04 - IMP - CU05 - Implement components - DS #done #time 1d 14m Implementing CU05 Command Detail

---
+++ app/src/main/java/es/unex/dcadmin/command/CommandAdapter.java
@@ -0,0 +1,141 @@
+package es.unex.dcadmin.command;
+
+
+import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.CheckBox;
+        import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+import es.unex.dcadmin.R;
+
+public class CommandAdapter extends RecyclerView.Adapter<CommandAdapter.ViewHolder> {
+    private List<Command> mItems = new ArrayList<Command>();
+
+    public interface OnItemClickListener {
+        void onItemClick(Command item);     //Type of the element to be returned
+    }
+
+    private final OnItemClickListener listener;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CommandAdapter(OnItemClickListener listener) {
+
+        this.listener = listener;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,//Parent es la vista padre de este elemento, en este caso la recyclerview, es el que contiene este elemento
+                                                     int viewType) {
+        //TODO - Inflate the View for every element
+        //A partir de un layout, le metemos los datos a la vista(lo mismo que hacíamos con los fragments, crear una vista)
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.command_item,parent,false);//LayoutInflater solo se puede crear con from. Esto mete los datos en el layout todo_item
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(mItems.get(position),listener);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void add(Command item) {
+
+        mItems.add(item);
+        notifyDataSetChanged();
+
+    }
+
+    public void clear(){
+
+        mItems.clear();
+        notifyDataSetChanged();
+
+    }
+
+    public Object getItem(int pos) {
+
+        return mItems.get(pos);
+
+    }
+
+    public void load(List<Command> items){
+
+        mItems.clear();
+        mItems = items;
+        notifyDataSetChanged();
+
+    }
+
+    public void update(Command item){
+        int pos = -1;
+        boolean found = false;
+        for(int i = 0;i<mItems.size() && !found; i++){
+            if(mItems.get(i).getId() == item.getId()){
+                pos = i;
+                found = true;
+            }
+        }
+
+        if(pos != -1)
+            mItems.set(pos,item);
+
+        notifyDataSetChanged();
+
+    }
+
+    public void delete(Command item){
+        int pos = mItems.indexOf(item);
+        mItems.remove(item);
+
+        notifyItemRemoved(pos);
+        notifyItemRangeChanged(pos,mItems.size());
+
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private ImageView deleteButton;
+
+        public ViewHolder(View itemView) {//itemVIew es la vista que contiene a todos los elementos
+            super(itemView);
+
+            //TODO - Get the references to every widget of the Item View
+            name = itemView.findViewById(R.id.commandName);
+            deleteButton = itemView.findViewById(R.id.deleteCommand);
+
+        }
+
+        public void bind(final Command toDoItem, final OnItemClickListener listener) {
+            //Vamos a vincular las vistas con los datos de toDoItem
+            //TODO - Display Title in TextView
+            name.setText(toDoItem.getName());
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(toDoItem);
+                }
+            });
+        }
+    }
+
+}
---
+++ app/src/main/java/es/unex/dcadmin/command/CommandDetail.java
@@ -0,0 +1,102 @@
+package es.unex.dcadmin.command;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import es.unex.dcadmin.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CommandDetail#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CommandDetail extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public static final String ARG_PARAM1 = "param1";
+    public static final String ARG_PARAM2 = "param2";
+    public static final String ARG_PARAM3 = "param3";
+    public static final String ARG_PARAM4 = "param4";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private String mParam3;
+    private String mParam4;
+
+    private EditText mTitleText;
+    private EditText mTriggerText;
+    private EditText mActionText;
+
+    private Command command;
+
+    public CommandDetail() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CommandDetail.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CommandDetail newInstance(String param1, String param2, String param3, String param4) {
+        CommandDetail fragment = new CommandDetail();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM3, param3);
+        args.putString(ARG_PARAM4, param4);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            command = new Command(bundle.getLong(ARG_PARAM2),bundle.getString(ARG_PARAM1), bundle.getString(ARG_PARAM3), bundle.getString(ARG_PARAM4));
+        }
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getString(ARG_PARAM3);
+            mParam4 = getArguments().getString(ARG_PARAM4);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.command_detail, container, false);
+        mTitleText = (EditText) v.findViewById(R.id.command_name);
+
+        mTriggerText = (EditText) v.findViewById(R.id.command_trigger);
+
+        mActionText = (EditText) v.findViewById(R.id.command_action);
+
+        mTitleText.setText(command.getName());
+
+        mTriggerText.setText(command.getTrigger_text());
+
+        mActionText.setText(command.getAction_text());
+
+        ImageView imageView = v.findViewById(R.id.saveView);
+
+        return v;
+    }
+}
