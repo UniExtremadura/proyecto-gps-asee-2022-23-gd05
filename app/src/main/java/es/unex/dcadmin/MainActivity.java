7d8e24a3aef619dab77ddbfc43facfd0b95cf220
Raúl <raulcascoseco@gmail.com>
2022-11-21 11:20:28 +0100

    PGD05-36 IT02 - IMP - CU01 - Implement Components - DS #done #time 1d 14m Implementation of CU01

--- app/src/main/java/es/unex/dcadmin/MainActivity.java
+++ app/src/main/java/es/unex/dcadmin/MainActivity.java
@@ -1,23 +1,44 @@
 package es.unex.dcadmin;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs;
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs  = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        setContentView(R.layout.activity_add_bot_token);
 
+        token = prefs.getString("token", "");
+
+        EditText tokenEditText = findViewById(R.id.addTokenView);
+
+        if(!token.equals("")) {
+            tokenEditText.setText(token);
+        }else{
+            tokenEditText.setText("");
+            tokenEditText.setHint("Token...");
+        }
+
+        ImageView access = findViewById(R.id.access);
+
+        access.setOnClickListener(view -> {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("token", tokenEditText.getText().toString());
+            editor.commit();
+        });
 
     }
 
